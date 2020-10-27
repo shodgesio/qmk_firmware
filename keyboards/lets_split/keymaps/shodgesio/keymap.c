01d7b943817b250874827ec5df99e53672ab4073
Stephen Hodges <stephen@shodges.io>
2020-10-27 09:33:06 -0400

    updating keymap for shodgesio to switch ctrl and alt

--- keyboards/lets_split/keymaps/shodgesio/keymap.c
+++ keyboards/lets_split/keymaps/shodgesio/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,   KC_I,     KC_O,     KC_P,     KC_BSPC,  \
   KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,    KC_G,    KC_H,    KC_J,   KC_K,     KC_L,     KC_SCLN,  KC_ENT,   \
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT , \
-  ADJUST,   KC_LGUI,  KC_LCTL,  KC_LALT,  LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_RALT,  KC_RCTL,  KC_MPLY,  KC_MNXT   \
+  ADJUST,   KC_LGUI,  KC_LALT,  KC_LCTL,  LOWER,   KC_SPC,  KC_SPC,  RAISE,  KC_RCTL,  KC_RALT,  KC_MPLY,  KC_MNXT   \
 ),
 
 /* Colemak
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRAVE,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_BSLS,  \
   KC_DEL,    KC_UNDS,  KC_PLUS,  _______,  _______,  _______,  _______,  KC_LPRN,  KC_RPRN,  _______,  KC_MINS,  KC_QUOT,  \
   KC_NLCK,   KC_MINS,  KC_EQL,   _______,  _______,  _______,  KC_EQL,   KC_LCBR,  KC_RCBR,  _______,  KC_PLUS,  KC_GRV,   \
-  _______,   _______,  _______,  KC_LALT,  _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_DOT,   KC_ENT,   _______   \
+  _______,   _______,  _______,  KC_LCTL,  _______,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_DOT,   KC_ENT,   _______   \
 ),
 
 /* Raise: featuring vim-style hjkl arrow keys
